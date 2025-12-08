4a5dd41894b05b91ba84d8738e222cbbc766a808
AngelicaGFlores <flores.guada.angelica@gmail.com>
2025-12-07 21:25:58 -0800

    added the backend link in api

--- src/clients/api.ts
+++ src/clients/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const apiClient = axios.create({
-	baseURL: "http://localhost:4000",
+	baseURL: import.meta.env.VITE_BACKEND_URL,
 	headers: {
 		Authorization:
 			"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7Il9pZCI6IjY5MzY1NzczZGE1MjAxNWY1YTRlOWUxMyIsInVzZXJuYW1lIjoiYW5nZWxpY2FmIiwiZW1haWwiOiJhbmdlbGljYUBnbWFpbC5jb20iLCJyb2xlIjoidXNlciJ9LCJpYXQiOjE3NjUxNjkwMjgsImV4cCI6MTc2NTE3NjIyOH0.E2jlDrtyn5c880WpjKgxtO0Ef34PoJhpQTunPd94X1k",
