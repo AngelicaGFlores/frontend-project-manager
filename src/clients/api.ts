78c50b9e3c9c4194bec7a3b22ccd3ca390814f43
AngelicaGFlores <flores.guada.angelica@gmail.com>
2025-12-09 15:32:27 -0800

    basic UI from class part3

--- src/clients/api.ts
+++ src/clients/api.ts
@@ -4,6 +4,6 @@ export const apiClient = axios.create({
 	baseURL: import.meta.env.VITE_BACKEND_URL,
 	headers: {
 		Authorization:
-			"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7Il9pZCI6IjY5MzY1NzczZGE1MjAxNWY1YTRlOWUxMyIsInVzZXJuYW1lIjoiYW5nZWxpY2FmIiwiZW1haWwiOiJhbmdlbGljYUBnbWFpbC5jb20iLCJyb2xlIjoidXNlciJ9LCJpYXQiOjE3NjUyNjA4NTQsImV4cCI6MTc2NTM0NzI1NH0.tnfnLd5ObTtoqktrAi3jDkk9DgYMAWyxANqa-REZflU",
+			"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJkYXRhIjp7Il9pZCI6IjY5Mzg2ZjA4NjY3MWMwNzU1ZWRlOWYwMiIsInVzZXJuYW1lIjoidXNlcmFuZyIsImVtYWlsIjoidXNlcmFuZ0BnbWFpbC5jb20iLCJyb2xlIjoidXNlciJ9LCJpYXQiOjE3NjUzMjMwMTksImV4cCI6MTc2NTQwOTQxOX0.gNJ_3lnR9QOTXYNcaUlFfZQ9EDDfyxxHXYH2uLv57uk",
 	},
 });
